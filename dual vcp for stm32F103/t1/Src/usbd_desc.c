82a3e0da0335ae74d7c39991903597252a5d8ca3
Jim <Jim>
2017-10-24 10:59:33 +0800

    change vid and description

--- dual vcp for stm32F103/t1/Src/usbd_desc.c
+++ dual vcp for stm32F103/t1/Src/usbd_desc.c
@@ -71,14 +71,14 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */ 
-#define USBD_VID     1155
+#define USBD_VID     0xDEFA
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "Baicells"
+#define USBD_PID_FS     0x9361
+#define USBD_PRODUCT_STRING_FS     "Baicells ATE ComPort"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
-#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
-#define USBD_INTERFACE_STRING_FS     "CDC Interface"
+#define USBD_CONFIGURATION_STRING_FS     "Dual Vcp Config"
+#define USBD_INTERFACE_STRING_FS     "Dual Vcp Interface"
 
 /* USER CODE BEGIN 0 */
 
